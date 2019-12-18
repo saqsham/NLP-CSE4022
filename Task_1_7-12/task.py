6237ff9c38a006124bf51fb9252112213183e555
saqsham <saqsham@gmail.com>
2019-12-18 07:34:52 +0530

    first

---
+++ Task_1_7-12/task.py
@@ -0,0 +1,39 @@
+# A sample text taken from William Shakesspeare's Wikipedia
+# link: https://en.wikipedia.org/wiki/William_Shakespeare
+import nltk
+from nltk.corpus import indian
+
+# Getting the info and assigning a variable
+ws = """
+        William Shakespeare was the son of John Shakespeare, an alderman and a successful 
+        glover (glove-maker) originally from Snitterfield, and Mary Arden, the daughter of 
+        an affluent landowning farmer. He was born in Stratford-upon-Avon and baptised 
+        there on 26 April 1564. His actual date of birth remains unknown, but is 
+        traditionally observed on 23 April, Saint George's Day. This date, which can be 
+        traced to a mistake made by an 18th-century scholar, has proved appealing to 
+        biographers because Shakespeare died on the same date in 1616. He was the 
+        third of eight children, and the eldest surviving son.
+    """
+
+# Same piece of text but in my native language
+wsh = """
+        विलियम शेक्सपियर, जॉन शेक्सपियर तथा मेरी आर्डेन के ज्येष्ठ पुत्र एवं तीसरी संतान थे। इनका जन्म स्ट्रैटफोर्ड आन एवन में 
+        हुआ। बाल्यकाल में उनकी शिक्षा स्थानीय फ्री ग्रामर स्कूल में हुई। पिता की बढ़ती हुई आर्थिक कठिनाइयों के कारण उन्हें पाठशाला 
+        छोड़कर छोटे मोटे धंधों में लग जाना पड़ा। जीविका के लिए उन्होंने लंदन जाने का निश्चय किया। इस निश्चय का एक दूसरा कारण 
+        भी था। कदाचित् चार्ल कोट के जमींदार सर टामस लूसी के उद्यान से हिरण की चोरी की ओर कानूनी कार्यवाही के भय से उन्हें 
+        अपना जन्मस्थान छोड़ना पड़ा। उनका विवाह सन् १५८२ में एन हैथावे से हो चुका था। सन् १५८५ के लगभग शेक्सपियर लंदन आए। 
+        शुरू में उन्होंने एक रंगशाला में किसी छोटी नौकरी पर काम किया, किंतु कुछ दिनों के बाद वे लार्ड चेंबरलेन की कंपनी के सदस्य बन 
+        गए और लंदन की प्रमुख रंगशालाओं में समय समय पर अभिनय में भाग लेने लगे। ग्यारह वर्ष के उपरांत सन् १५९६ में ये स्ट्रैटफोर्ड 
+        आन एवन लौटे और अब इन्होंने अपने परिवार की आर्थिक व्यवस्था सुदृढ़ बना दी। सन् १५९७ में इन्होंने धीरे धीरे नवनिर्माण एवं विस्तार 
+        किया। इसी भवन में सन् १६१० के बाद वे अपना अधिकाधिक समय व्यतीत करने लगे और वहीं सन् १६१६ में उनका देहांत हुआ। 
+    """ 
+ 
+
+# printing and observing the output
+print(ws)
+
+# printing and observing the output
+print(wsh)
+
+# Obesrvations in Task-1.md
+print(nltk.corpus.indian.words('hindi.pos'))
